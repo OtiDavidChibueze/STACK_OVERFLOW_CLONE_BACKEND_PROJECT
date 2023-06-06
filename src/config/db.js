a786a235f6b1d0e4075e86ce8ea51ed5aa951a27
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-06-06 21:40:17 +0100

    Create db.js

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+// DATABASE CONNECTION
+const mongoose = require("mongoose");
+const { logger } = require("../config/logger");
+const { DB } = require("../config/keys");
+
+const connectToDatabase = () => {
+  try {
+    mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    logger.info("database connected successfully");
+  } catch (err) {
+    logger.error(`database not connected : ${JSON.stringify(err)}`);
+  }
+};
+
+module.exports = connectToDatabase;
