2ce5ccb19734e38ba8855dd2fc1a80e48c2c5bfc
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-06-07 11:03:17 +0100

    Update helperFunction.js

--- src/util/helperFunction.js
+++ src/util/helperFunction.js
@@ -1,10 +1,24 @@
 const bcrypt = require("bcrypt");
 
 class HelperFunction {
+  /**
+   * @description - THIS IS USED TO HASH A PASSWORD
+   * @param {string} password - THE PASSWORD TO BE HASHED
+   * @returns - RETURNS THE METHODS TO HASH PASSWORD
+   * @memberof HelperFunction
+   */
+
   static async hashPassword(password) {
     return await bcrypt.hash(password, await bcrypt.genSalt());
   }
 
+  /**
+   * @description - THIS IS USED TO COMPARE PASSWORD
+   * @param {string} password - THE PROVIDED PASSWORD
+   * @param {string} hashedPassword - THE HASHED PASSWORD TO BE COMPARED WITH
+   * @returns - RETURNS THE METHODS TO COMPARE THE PASSWORD
+   * @memberof HelperFunction
+   */
   static async comparePassword(password, hashedPassword) {
     return await bcrypt.compare(password, hashedPassword);
   }
