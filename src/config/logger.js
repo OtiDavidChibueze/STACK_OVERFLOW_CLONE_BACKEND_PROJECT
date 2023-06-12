ac81319d2cc31726790a0bb681e4e9862d6131c9
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-06-12 23:52:59 +0100

    Update logger.js

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,53 +1,29 @@
 const { createLogger, transports, format } = require("winston");
-const expressWinston = require("express-winston");
-
-const customFormat = format.combine(
-  format.json(),
-  format.prettyPrint(),
-  format.timestamp(),
-  format.printf(({ level, message, timestamp }) => {
-    return `[${new Date(timestamp).toUTCString()}] : [${level
-      .toUpperCase()
-      .padEnd(7)}] : ${message}`;
-  })
-);
 
 const logger = createLogger({
-  transports: [
-    new transports.Console(),
+  format: format.combine(
+    format.json(),
+    format.timestamp({ format: "ddd, DD MMM YYYY HH:mm:ss [GTM]" }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.printf(({ timestamp, level, message }) => {
+      return `[${timestamp}] - [${level.toUpperCase()}] - ${message}`;
+    })
+  ),
 
-    new transports.File({ level: "warn", filename: "appDevelopmentError.log" }),
-    new transports.File({
-      level: "error",
-      filename: "appDevelopmentError.log",
+  transports: [
+    new transports.Console({
+      format: format.combine(format.colorize(), format.simple()),
     }),
-    new transports.File({
-      level: "silent",
-      filename: "appDevelopmentSilent.log",
-    }),
+
+    new transports.File({ filename: "appDevelopment.log" }),
   ],
-  format: customFormat,
+
+  exceptionHandlers: [
+    new transports.File({ filename: "exceptionDevelopment.log" }),
+  ],
+
+  exitOnError: false,
 });
 
-const errorFormat = format.combine(
-  format.json(),
-  format.prettyPrint(),
-  format.timestamp(),
-  format.printf(({ level, meta, timestamp }) => {
-    return `[${new Date(
-      timestamp
-    ).toUTCString()}] : [${level.toUpperCase()}] : ${meta.message}`;
-  })
-);
-
-const internalErrorLogger = expressWinston.errorLogger({
-  transports: [
-    new transports.File({
-      level: "error",
-      filename: "appDevelopmentInternalError.log",
-    }),
-  ],
-  format: errorFormat,
-});
-
-module.exports = { logger, internalErrorLogger };
+module.exports = logger;
