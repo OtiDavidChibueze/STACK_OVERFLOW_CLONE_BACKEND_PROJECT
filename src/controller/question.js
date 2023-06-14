173e65d742dc39aceb2784a466b467e27f5a21b9
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-06-14 11:24:48 +0100

    Create question controller

---
+++ src/controller/question.js
@@ -0,0 +1,182 @@
+// QUESTION CONTROLLER
+const QuestionService = require("../service/question");
+const { errorResponse, successResponse } = require("../util/responseHelper");
+const logger = require("../config/logger");
+
+class QuestionController {
+  /**
+   * @description - RETURNS A JSON OBJECT
+   * @param {object} req - THE REQUEST OBJECT
+   * @param {object} res - THE RESPONSE OBJECT
+   * @returns - RETURNS JSON OBJECT
+   * @memberof QuestionController
+   */
+
+  static async getAllQue(req, res) {
+    try {
+      const result = await QuestionService.getAllQuestions();
+
+      if (result == 409)
+        return errorResponse(res, result.statusCode, result.message);
+
+      logger.info(`Gotten all questions : ${JSON.stringify(result)}`);
+
+      return successResponse(
+        res,
+        result.statusCode,
+        result.message,
+        result.data
+      );
+    } catch (err) {
+      logger.error(
+        `Error found while getting questions : ${JSON.stringify(err.message)}`
+      );
+      return errorResponse(res, 409, "Oops something went wrong..");
+    }
+  }
+
+  /**
+   *@description - RETURNS A JSON OBJECT
+   * @param {object} req - THE REQUEST OBJECT
+   * @param {object} res - THE RESPONSE OBJECT
+   * @returns - RETURNS JSON OBJECT
+   * @memberof QuestionController
+   */
+
+  static async askQue(req, res) {
+    if (req.user.role !== "user") {
+      logger.error("Unauthorized Access");
+      return errorResponse(res, 401, "UnAuthorized Access");
+    }
+    try {
+      const { body } = req;
+
+      const result = await QuestionService.askQuestion(body, req);
+      if (result.statusCode == 409)
+        return errorResponse(res, result.statusCode, result.message);
+
+      logger.info(
+        `question submitted successfully : ${JSON.stringify(result)}`
+      );
+      return successResponse(
+        res,
+        result.statusCode,
+        result.message,
+        result.data
+      );
+    } catch (err) {
+      logger.error(
+        `Error found while asking question at question controller : ${JSON.stringify(
+          err.message
+        )}`
+      );
+      return errorResponse(res, 500, "Oops something went wrong");
+    }
+  }
+
+  /**
+   *@description - RETURNS A JSON OBJECT
+   * @param {object} req - THE REQUEST OBJECT
+   * @param {object} res - THE RESPONSE OBJECT
+   * @returns - RETURNS JSON OBJECT
+   * @memberof QuestionController
+   */
+
+  static async voteUpQues(req, res) {
+    if (req.user.role !== "user") {
+      logger.error("Unauthorized Access");
+      return errorResponse(res, 401, "UnAuthorized Access");
+    }
+
+    try {
+      const { params } = req;
+
+      const result = await QuestionService.voteUpQuestion(params, req);
+
+      if (result.statusCode == 409)
+        return errorResponse(res, result.statusCode, result.message);
+
+      logger.info(`user voted up : ${JSON.stringify(result)} `);
+      return successResponse(
+        res,
+        result.statusCode,
+        result.message,
+        result.data
+      );
+    } catch (err) {
+      logger.error(
+        `Error found while voting up question at question controller : ${JSON.stringify(
+          err.message
+        )}`
+      );
+      return errorResponse(res, 500, "Oops something went wrong");
+    }
+  }
+
+  /**
+   *@description - RETURNS A JSON OBJECT
+   * @param {object} req - THE REQUEST OBJECT
+   * @param {object} res - THE RESPONSE OBJECT
+   * @returns - RETURNS JSON OBJECT
+   * @memberof QuestionController
+   */
+
+  static async voteDownQue(req, res) {
+    if (req.user.role !== "user")
+      return errorResponse(res, 401, "Unauthorized Access");
+
+    try {
+      const { params } = req;
+
+      const result = await QuestionService.voteDownQuestion(params, req);
+
+      if (result == 409)
+        return errorResponse(res, result.statusCode, result.message);
+
+      logger.info("Question Controller -> info : user voted down");
+
+      return successResponse(
+        res,
+        result.statusCode,
+        result.message,
+        result.data
+      );
+    } catch (err) {
+      logger.error(`Question Controller -> Error : ${err.message}`);
+      return errorResponse(res, 406, "vote removed already");
+    }
+  }
+
+  /**
+   * @description -  RETURNS A JSON OBJECT
+   * @param {object} req - THE REQUEST OBJECT
+   * @param {object} res - THE RESPONSE OBJECT
+   * @returns - RETURNS A JSON OBJECT
+   * @memberof QuestionController
+   */
+
+  static async answerQue(req, res) {
+    if (req.user.role !== "user")
+      return errorResponse(res, 401, "Unauthorized Access");
+
+    try {
+      const result = await QuestionService.answerQuestion(req.params, req);
+
+      if (result.statusCode == 409)
+        return errorResponse(res, result.statusCode, result.message);
+
+      logger.info(`Question Controller -> Info : ${result.data} `);
+      return successResponse(
+        res,
+        result.statusCode,
+        result.message,
+        result.data
+      );
+    } catch (err) {
+      logger.error(`Question Controller -> Error : ${err.message}`);
+      return errorResponse(res, 500, "Oops something went wrong");
+    }
+  }
+}
+
+module.exports = QuestionController;
